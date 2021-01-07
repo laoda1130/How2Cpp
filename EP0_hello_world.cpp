615f5a00bbda00529e2cecb2906b5b27f32f493d
laoda1130 <76833637+laoda1130@users.noreply.github.com>
2021-01-07 12:00:30 +0800

    Add files via upload

---
+++ EP0_hello_world.cpp
@@ -0,0 +1,7 @@
+#include<iostream>        //加入欲使用的函式庫
+using namespace std;      //訂定命名空間
+int main() {
+	cout << "Hello world";//打印結果
+	system("pause");      //呼叫windows系統，凍結視窗
+	return 0;             //函式回傳
+}
